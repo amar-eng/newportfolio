929767aad1f10a4a88ddeff19e89f2e35929fb41
amar-eng <am24mo@gmail.com>
2021-09-03 01:06:04 -0400

    changes to the repsoniveness of the contact page

--- src/pages/Contactpage.js
+++ src/pages/Contactpage.js
@@ -19,103 +19,106 @@ const Contactpage = () => {
           </div>
           <div className="contact-flex">
             <div className="  contact-row">
-              <div className="contact-form">
-                <form>
-                  <div className="form-container">
-                    <div className="input__group">
-                      <input
-                        type="text"
-                        placeholder="Name"
-                        className="inputControl"
-                      />
-                    </div>
-                    <div className="input__group">
-                      <input
-                        type="text"
-                        placeholder="Email"
-                        className="inputControl"
-                      />
-                    </div>
-                    <div className="input__group">
-                      <input
-                        type="text"
-                        placeholder="Subject"
-                        className="inputControl"
-                      />
-                    </div>
-                    <div class="input__group">
-                      <textarea
-                        placeholder="Message"
-                        className="inputControl"
-                      ></textarea>
-                    </div>
-                    <div class="submit-btn">
-                      <button type="submit" className="btn contact-btn">
-                        send message
-                      </button>
-                    </div>
+              <form>
+                <div className="form-container">
+                  <div className="input__group">
+                    <input
+                      type="text"
+                      placeholder="Name"
+                      className="inputControl"
+                    />
                   </div>
-                </form>
-              </div>
+                  <div className="input__group">
+                    <input
+                      type="text"
+                      placeholder="Email"
+                      className="inputControl"
+                    />
+                  </div>
+                  <div className="input__group">
+                    <input
+                      type="text"
+                      placeholder="Subject"
+                      className="inputControl"
+                    />
+                  </div>
+                  <div class="input__group">
+                    <textarea
+                      placeholder="Message"
+                      className="inputControl"
+                    ></textarea>
+                  </div>
+                  <div class="submit-btn submit__btn">
+                    <button type="submit" className="btn contact-btn buttons">
+                      send message
+                    </button>
+                  </div>
+                </div>
+              </form>
             </div>
             <div className="contact-info info-row">
-              {/* <h1 className="contact__header">Let's Talk</h1> */}
               <div className="contact">
                 <div className="contact__info">
                   <div className="contact__info-header">Contact info</div>
-                  <div className="contact__info-content">
-                    Feel feel to reach out!
-                  </div>
-                  <div className="icon-text">
-                    <BiPhone
-                      style={{
-                        fontSize: "1.8rem",
-                        color: "#13182c",
-                        marginRight: "2rem",
-                      }}
-                    />
-                    <span className="span-content">1-(647)-767-8903</span>
-                  </div>
-                  <div className="icon-text">
-                    <HiOutlineMail
-                      style={{
-                        fontSize: "1.8rem",
-                        color: "#13182c",
-                        marginRight: "2rem",
-                      }}
-                    />
-                    <span className="span-content">am24mo@gmail.com</span>
-                  </div>
-                  <div className="socials">
-                    <a className="icon-circle">
-                      <AiFillGithub
-                        style={{
-                          color: "#4078c0",
-                          fontSize: "30px",
-                          cursor: "pointer",
-                        }}
-                        onClick={() =>
-                          window.open(`https://github.com/amar-eng`)
-                        }
-                      />
-                    </a>
-                    <a className="icon-circle">
-                      <AiFillLinkedin
-                        style={{
-                          color: " #0e76a8",
-                          fontSize: "30px",
-                          cursor: "pointer",
-                        }}
-                        onClick={() =>
-                          window.open(
-                            `https://www.linkedin.com/in/amar-mohamud/`
-                          )
-                        }
-                      />
-                    </a>
-                  </div>
-                  <div className="contact-logo">
-                    <img src={logo} alt="my-logo" className="logo-img" />
+                  <div className="contact-Container">
+                    <div className="contact-Container--Left">
+                      <div className="contact__info-content">
+                        Feel feel to reach out!
+                      </div>
+                      <div className="icon-text">
+                        <BiPhone
+                          style={{
+                            fontSize: "1.8rem",
+                            color: "#13182c",
+                            marginRight: "2rem",
+                          }}
+                        />
+                        <span className="span-content">1-(647)-767-8903</span>
+                      </div>
+                      <div className="icon-text">
+                        <HiOutlineMail
+                          style={{
+                            fontSize: "1.8rem",
+                            color: "#13182c",
+                            marginRight: "2rem",
+                          }}
+                        />
+                        <span className="span-content">am24mo@gmail.com</span>
+                      </div>
+                    </div>
+                    <div className="socials">
+                      <div className="socials__container">
+                        <a className="icon-circle">
+                          <AiFillGithub
+                            style={{
+                              color: "#4078c0",
+                              fontSize: "30px",
+                              cursor: "pointer",
+                            }}
+                            onClick={() =>
+                              window.open(`https://github.com/amar-eng`)
+                            }
+                          />
+                        </a>
+                        <a className="icon-circle">
+                          <AiFillLinkedin
+                            style={{
+                              color: " #0e76a8",
+                              fontSize: "30px",
+                              cursor: "pointer",
+                            }}
+                            onClick={() =>
+                              window.open(
+                                `https://www.linkedin.com/in/amar-mohamud/`
+                              )
+                            }
+                          />
+                        </a>
+                      </div>
+                      <div className="contact__logo">
+                        <img src={logo} alt="my-logo" className="logo__img" />
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
