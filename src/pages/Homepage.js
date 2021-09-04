5f65a1c93e2bc3fd84e7402d4fa04503617e0130
amar-eng <am24mo@gmail.com>
2021-09-04 15:45:34 -0400

    finishing some cleanup

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,22 +1,10 @@
-import React, { useEffect, useRef } from "react";
-import { init } from "ityped";
+import React from "react";
+
 import Navbar from "../components/Navbar";
 import logo from "../assets/pics/logo.png";
 
 import "./Styles.scss";
 const Homepage = () => {
-  const textRef = useRef();
-  useEffect(() => {
-    init(textRef.current, {
-      showCursor: false,
-      backDelay: 1000,
-      startDelay: 500,
-      cursorChar: "|",
-      backSpeed: 50,
-      typeSpeed: 120,
-      strings: ["Front Developer", "Mentor", "Entrepreneur", "Colognoisseur"],
-    });
-  }, []);
   return (
     <div className="main">
       <section className="home-sections">
@@ -26,7 +14,7 @@ const Homepage = () => {
             <div className="home__text">
               {/* <p>Hi, I'm a</p> */}
               {/* <h1>Amar Mohamud</h1> */}
-              {/* <h2 ref={textRef}></h2> */}
+
               <h2>Front End Developer</h2>
               <a href="#" className="bttn">
                 About me
